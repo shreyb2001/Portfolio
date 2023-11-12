887a102f924cf30f9c5813f130267c691e55677f
shreyb2001 <123163163+shreyb2001@users.noreply.github.com>
2023-11-12 17:54:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -43,14 +43,7 @@ function App() {
                 position={[-0.5, 1, 4]}
                 distance={10}
               />
-              <Stars
-                radius={100}
-                depth={50}
-                count={5000}
-                factor={4}
-                saturation={0}
-                fade
-                speed={1}
+              <color attach="background" args={["black"]} />
               />
             </>
           ) : (
